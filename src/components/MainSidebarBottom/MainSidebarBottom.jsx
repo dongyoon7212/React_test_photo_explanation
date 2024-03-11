144a5c16e6b0d5814bd2cec4ec2c62fdcd040736
dongyoon7212 <dongyoon7212@naver.com>
2024-03-11 12:21:31 +0900

    init: first commit

--- src/components/MainSidebarBottom/MainSidebarBottom.jsx
+++ src/components/MainSidebarBottom/MainSidebarBottom.jsx
@@ -1,5 +1,4 @@
 /** @jsxImportSource @emotion/react */
-import { Link } from "react-router-dom";
 import * as S from "./style";
 import { SIDEBAR_MENUS } from "../../constants/menu";
 import MainSidebarMenu from "../MainSidebarMenu/MainSidebarMenu";
@@ -7,12 +6,14 @@ import MainSidebarMenu from "../MainSidebarMenu/MainSidebarMenu";
 function MainSidebarBottom() {
     return (
         <ul css={S.layout}>
-            {
-                SIDEBAR_MENUS.map(
-                    menu => 
-                    <MainSidebarMenu path={menu.path} icon={menu.icon} name={menu.name}/>
-                )
-            }
+            {SIDEBAR_MENUS.map((menu) => (
+                <MainSidebarMenu
+                    key={menu.id}
+                    path={menu.path}
+                    icon={menu.icon}
+                    name={menu.name}
+                />
+            ))}
         </ul>
     );
 }
